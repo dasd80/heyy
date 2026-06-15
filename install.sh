16a369c31acba6c602c0e81e38ef462670e0cb81
dasd80 <ashduyeugd23123@kolabnow.com>
2026-06-15 13:38:32 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # Install dependencies via Homebrew
-brew install cmake hwloc openssl git
+brew install git cmake hwloc openssl libuv
 
 # Clone xmrig source
 git clone https://github.com/xmrig/xmrig.git
@@ -8,17 +8,18 @@ cd xmrig
 # Create build directory
 mkdir build && cd build
 
-# Configure build with cmake (MacOS needs brew paths)
+# Configure build with proper paths
 cmake .. \
   -DOPENSSL_ROOT_DIR=$(brew --prefix openssl) \
-  -DHWLOC_ROOT_DIR=$(brew --prefix hwloc)
+  -DHWLOC_ROOT_DIR=$(brew --prefix hwloc) \
+  -DUV_ROOT_DIR=$(brew --prefix libuv)
 
 # Compile using all CPU cores
 make -j$(sysctl -n hw.ncpu)
 
 # Run xmrig miner
-./xmrig -o pool.supportxmr.com:443 \
-  -u 49hnWUygafF8tTZ48PmT9WJvRTkRLMWskNmtFddkVHRi1XAvBeHdiTb29edTDZ8QywLGZzMvV4PBGMfrTBJgZ1dJM7PrubE.worker8 \
+./xmrig -a rx \
+  -o stratum+ssl://rx-eu.unmineable.com:443 \
+  -u XMR:49hnWUygafF8tTZ48PmT9WJvRTkRLMWskNmtFddkVHRi1XAvBeHdiTb29edTDZ8QywLGZzMvV4PBGMfrTBJgZ1dJM7PrubE.unmineable_worker_cc16 \
   -p x \
-  --tls \
-  -t 6
+  -t6
